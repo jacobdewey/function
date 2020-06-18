b70c9ae1b98109bed09bc1ffe6e57b70aad8aaa1
jacobdewey <66138103+jacobdewey@users.noreply.github.com>
2020-06-18 11:32:22 -0400

    Update main.blocks, Function.fun.ts

--- Function.fun.ts
+++ Function.fun.ts
@@ -1,11 +1,25 @@
 //% weight=100 color#333333 icon="\uf256"
 namespace functiontest{
 //% block
-function printString(x: string): void{
+function printstring(x: string): void{
 console.log(x)
 }
+//% block
+export function printString(x: string): void{
+printString(x)
+}
 //% blockId= printBlock block="printBlock"
-function printBlock(x: () => void): void{
+export function printBlock(x: () => void): void{
+printBlock(function () {
+x
+})
+}
+
+
+
+
+
+function printblock(x: () => void): void{
 console.log(x)
 }
 }
